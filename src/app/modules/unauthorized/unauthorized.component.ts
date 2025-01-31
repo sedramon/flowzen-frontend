e2b9563e66843ab44ef9e3000a67b89381c0d56e
Aljosa Gadjanski Bozic <aljosagadjanskibozic@gmail.com>
2025-01-31 03:09:09 +0100

    Style configuration

--- src/app/modules/unauthorized/unauthorized.component.ts
+++ src/app/modules/unauthorized/unauthorized.component.ts
@@ -3,17 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-unauthorized',
   template: `
-    <div class="unauthorized">
+    <div class="main-container unauthorized">
       <h1>Access Denied</h1>
       <p>You do not have permission to access this page.</p>
-      <a routerLink="/home" mat-button>Go to Home</a>
     </div>
   `,
   styles: [
     `
       .unauthorized {
         text-align: center;
-        margin-top: 50px;
       }
     `,
   ],
