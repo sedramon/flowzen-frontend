8cc14aba7a4848e81b7f68cef53dce964731e73d
Aljosa Gadjanski Bozic <aljosagadjanskibozic@gmail.com>
2025-02-08 02:45:18 +0100

    User administration mat table

--- src/app/modules/user-administration/user-administration.component.ts
+++ src/app/modules/user-administration/user-administration.component.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { UserAdministrationService } from './services/user-administration.service';
 import { User } from '../../models/User';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../core/services/auth.service';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatIconButton } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-user-administration',
   standalone: true,
-  imports: [FlexLayoutModule, CommonModule],
+  imports: [FlexLayoutModule, CommonModule, MatPaginatorModule, MatTableModule, MatSortModule, MatIconModule, MatIconButton],
   templateUrl: './user-administration.component.html',
   styleUrl: './user-administration.component.scss'
 })
-export class UserAdministrationComponent implements OnInit {
+export class UserAdministrationComponent implements OnInit, AfterViewInit {
   users: User[] = [];
+
+  displayedColumnsUsers: string[] = ['name', 'email', 'role', 'tenant', 'actions'];
+  dataSourceUsers = new MatTableDataSource<User>(this.users);
+
+  @ViewChild(MatPaginator) paginatorUsers!: MatPaginator;
+  @ViewChild(MatSort) sortUsers!: MatSort;
 
   constructor(private userAdministrationService: UserAdministrationService, private authService: AuthService) { }
 
@@ -27,6 +39,7 @@ export class UserAdministrationComponent implements OnInit {
     this.userAdministrationService.fetchUsers(currentUser.tenant).subscribe({
       next: (data) => {
         console.log("Users fetched:", data);
+        this.dataSourceUsers.data = data;
       },
       error: (err) => {
         console.error("Error fetching users:", err);
@@ -37,6 +50,15 @@ export class UserAdministrationComponent implements OnInit {
     this.userAdministrationService.users$.subscribe((data) => {
       this.users = data;
     });
+  }
+
+  ngAfterViewInit(): void {
+      this.dataSourceUsers.paginator = this.paginatorUsers;
+      this.dataSourceUsers.sort = this.sortUsers;
+  }
+
+  deleteUser(user: User) {
+    console.log('Deleting user : ' + user.name);
   }
 
 }
